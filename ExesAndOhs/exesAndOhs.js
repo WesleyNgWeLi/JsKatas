3ba58829eb960eabe85208d2a7f1434c82a1aabe
wesleyngweli <wesleyngweli@gmail.com>
2018-01-18 11:19:40 -0600

    completed ExesAndOhs

--- ExesAndOhs/exesAndOhs.js
+++ ExesAndOhs/exesAndOhs.js
@@ -1,3 +1,14 @@
 const XO= (str) => {
-    //code here
+    const result = str.toUpperCase();
+    const xCount = result.match(/X/g)
+    const yCount = result.match(/O/g)
+    if(xCount === null && yCount === null)
+        return true
+    else if(xCount === null || yCount === null)
+        return false
+        
+    return(xCount.length === yCount.length) 
 }
+
+
+export  {XO}
--- ExesAndOhs/exesAndOhs.spec.js
+++ ExesAndOhs/exesAndOhs.spec.js
@@ -1,7 +1,26 @@
 import {expect} from 'chai'
+import {XO} from './exesAndOhs'
 
-describe('doSomething', ()=>{
-    it('should do something', ()=>{
-        expect(1).to.eql(1)
+describe('XO', ()=>{
+    it('should return a boolean', ()=>{
+        expect(XO("xxx")).to.be.a('boolean')
+    })
+
+    it('should return true when equal x and o', ()=>{
+        const result = XO('xxxooo')
+        const expectedResult = true
+        expect(result).to.eql(expectedResult)
+    })
+
+    it('should return false when unequal x and o', ()=>{
+        const result = XO('xxxxfdsooo')
+        const expectedResult = false
+        expect(result).to.eql(expectedResult)
+    })
+
+    it('should return true when no x and o', ()=>{
+        const result = XO('dsadsadsahgfdhgfd')
+        const expectedResult = true
+        expect(result).to.eql(expectedResult)
     })
 })
