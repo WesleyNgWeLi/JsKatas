9da02bf8e55e826eac67b705bf9cf59594c5d72a
wesleyngweli <wesleyngweli@gmail.com>
2018-01-18 10:54:05 -0600

    initial commit for ExesAndOhs

---
+++ ExesAndOhs/exesAndOhs.js
@@ -0,0 +1,3 @@
+const XO= (str) => {
+    //code here
+}
---
+++ ExesAndOhs/exesAndOhs.spec.js
@@ -0,0 +1,7 @@
+import {expect} from 'chai'
+
+describe('doSomething', ()=>{
+    it('should do something', ()=>{
+        expect(1).to.eql(1)
+    })
+})
